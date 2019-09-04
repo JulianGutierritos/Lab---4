e1bb89b1b374d033d095a1b0600b6ada6bf413b2
unknown <2155486@labinfo.is.escuelaing.edu.co>
2019-09-04 11:54:58 -0500

    Parte II

--- src/main/java/hangman/setup/guice/HangmanFactoryServices.java
+++ src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -11,12 +11,20 @@ package hangman.setup.guice;
  */
 
 import hangman.model.French;
+import hangman.model.English;
+import hangman.model.Spanish;
 import hangman.model.Language;
 import hangman.model.GameScore;
 import hangman.model.OriginalScore;
+import hangman.model.BonusScore;
+import hangman.model.PowerBonusScore;
 import hangman.model.dictionary.HangmanDictionary;
 import hangman.model.dictionary.FrenchDictionaryDataSource;
+import hangman.model.dictionary.SpanishDictionaryDataSource;
+import hangman.model.dictionary.EnglishDictionaryDataSource;
 import hangman.view.HangmanNoviolentoPanel;
+import hangman.view.HangmanStickmanPanel;
+import hangman.view.HangmanColoridoPanel;
 import hangman.view.HangmanPanel;
 
 public class HangmanFactoryServices extends com.google.inject.AbstractModule {
